7c6747ba6262c957e56c9aaf698ce4584bf263d4
mrstuff456 <115061061+mrstuff456@users.noreply.github.com>
2025-06-10 10:11:55 +1200

    menu visuals
    finished the layout for the menu that is shared by all pages

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 import logging
 
@@ -9,34 +9,34 @@ app.logger.setLevel(logging.DEBUG)
 
 
 
-def removehyphens(item):
-    #Ah yes Perry, you are just in time to witness my latest invention, the DEHYPHEN-INATOR!!!!!
+def removepluses(item):
+    #Ah yes Perry, you are just in time to witness my latest invention, the DEPLUS-INATOR!!!!!
 
-    rebuild = "" #Initialise DEHYPHEN-INATOR
+    rebuild = "" #Initialise DEPLUS-INATOR
 
-    #REMOVE ALL HYPHENS IN THE TRI-STATE AREA!!!
+    #REMOVE ALL PLUSES IN THE TRI-STATE AREA!!!
     for i in item:
-        if i == '-':
+        if i == '+':
             i = ' '
         rebuild += i
-    #GIVE THEM BACK THEIR HYPHENLESS WORLD!!! MWAH HA HA!!!!
+    #GIVE THEM BACK THEIR PLUSLESS WORLD!!! MWAH HA HA!!!!
     return rebuild
 
 
-def addhyphens(item):
-    #Ah but there is MORE perry, here is the REHYPHEN-INATOR!!!
+def addpluses(item):
+    #Ah but there is MORE perry, here is the REPLUS-INATOR!!!
 
-    rebuild = "" #Initialise REHYPHEN-INATOR
+    rebuild = "" #Initialise REPLUS-INATOR
 
-    #MAKE ALL SPACES HYPHENS IN THE TRI-STATE AREA!!!
+    #MAKE ALL SPACES PLUSES IN THE TRI-STATE AREA!!!
     for i in item:
         if i == ' ':
-            i = '-'
+            i = '+'
         rebuild += i
-    #GIVE THEM BACK THEIR SPACELESS WORLD!  MWAH HA HA!!!
+    #GIVE THEM BACK THEIR PLUSLESS WORLD!  MWAH HA HA!!!
     return rebuild
 
- 
+
 
 @app.route('/card/hermit/<name>/<rarity>') # Hermit stats page
 def hermit_page(name, rarity):
@@ -44,15 +44,12 @@ def hermit_page(name, rarity):
     conn =  sqlite3.connect('card.db')
     cur = conn.cursor()
 
-    #remove all hyphens from the url to query the database
-    name = removehyphens(name)
-    rarity = removehyphens(rarity)
-
     #query the database to get the "head" stats
     cur.execute(f"SELECT * FROM Card WHERE Name = '{name}' AND Rarity = '{rarity}';")
     HeadStats = cur.fetchall()
     id = HeadStats[0][0] #extract the id from the headstats
     title = f"{HeadStats[0][1]} {HeadStats[0][2]} - Details"
+
     #query the database to get the "hermit" stats
     cur.execute(f"SELECT * FROM Hermit WHERE id = {id}")
     HermitStats = cur.fetchall()
@@ -62,10 +59,21 @@ def hermit_page(name, rarity):
     SeeAlso = cur.fetchall()
 
 
-    return render_template('hermit_details.html', HeadStats = HeadStats, HermitStats = HermitStats, SeeAlso = SeeAlso, title = title)
+    #generate the Header of the page
+    Header = f"#{id} {HeadStats[0][1]} - {HeadStats[0][2]}"
+
+    #set the deddicated stylesheeet
+    stylesheet = "hermit_details.css"
+
+    return render_template('hermit_details.html', HeadStats = HeadStats, HermitStats = HermitStats, SeeAlso = SeeAlso, title = title, Header = Header, stylesheet=stylesheet)
 
 
-@app.route('/beemovie')
+@app.route('/search')
+def search():
+    return render_template('layout.html')
+
+
+@app.route('/beemovie') #thinking bee
 def beemovie():
     return render_template('beemovie.html')
 
